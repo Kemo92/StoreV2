8ac362d3458d3e7365ebed1bf6673772a84f6b31
Kemal Durakovic <durakovic.kemal11@gmail.com>
2015-06-12 18:01:30 -0500

    Initial Commit

---
+++ GameCollection/Games.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameCollection
+{
+
+    public class Games
+    {
+        private IList<Product> products;
+
+
+
+        public Games()
+        {
+            products = new List<Product>
+             {
+                new Product("MW2", 20.00m),
+                new Product("God of War", 40.00m)
+            };
+        }
+
+
+
+        public IEnumerable<Product> AllProducts()
+        {
+            return products;
+        }
+
+       
+ }
+   
+}
